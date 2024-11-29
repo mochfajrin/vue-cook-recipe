653a3c0dae1052a9a56c99abd6a29433880b6e67
homurin <mochfajrin1211@gmail.com>
2024-11-29 20:14:08 +0700

    feat: init commit

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <header></header>
+  <main class="mx-10 mt-10">
+    <section><p class="">Hello World</p></section>
+  </main>
+  <footer></footer>
+</template>
